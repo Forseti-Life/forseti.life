fa64e3eff8d637ecbf77e3750f9d0e0a86e1a6cf
Keith Aumiller <keith@forseti.life>
2025-12-18 14:41:19 -0500

    Fix Invariant Violation: hardcode app registration name
    - Changed from dynamic import to hardcoded 'forseti-mobile'
    - Importing from package.json doesn't work in production builds
    - Rebuild v1.0.3-debug with proper registration
    - This should fix the 'forseti-mobile has not been registered' error

--- forseti-mobile/index.js
+++ forseti-mobile/index.js
@@ -5,6 +5,5 @@
 import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 import App from './App';
-import { name as appName } from './package.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('forseti-mobile', () => App);
