f56a0e3d3e53758939558261fcbed491b88ed7ee
Keith Aumiller <keith.aumiller@stlouisintegration.com>
2026-01-13 19:08:20 +0000

    Fix login API to properly return session token

--- sites/forseti/web/modules/custom/amisafe/src/Controller/ApiController.php
+++ sites/forseti/web/modules/custom/amisafe/src/Controller/ApiController.php
@@ -1208,10 +1208,21 @@ class ApiController extends ControllerBase {
       // Log the user in and create a session
       user_login_finalize($user);
       
-      // Get session token
+      // Get session token - need to force session start and get the actual ID
+      $session_manager = \Drupal::service('session_manager');
+      $session_manager->start();
       $session_token = \Drupal::service('session')->getId();
+      
+      // If still empty, generate a session
+      if (empty($session_token)) {
+        $session_manager->regenerate();
+        $session_token = \Drupal::service('session')->getId();
+      }
 
-      \Drupal::logger('amisafe')->info('Mobile user logged in: @name', ['@name' => $data['name']]);
+      \Drupal::logger('amisafe')->info('Mobile user logged in: @name (session: @session)', [
+        '@name' => $data['name'],
+        '@session' => $session_token ? 'created' : 'empty'
+      ]);
 
       $response = new JsonResponse([
         'success' => TRUE,
